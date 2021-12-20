e497c92f4993e0e0bd2b296a10ba76c1477e1c57
Dmitry-Kim <itfwtf@gmail.com>
2021-12-20 17:22:02 +0600

    Styles, new options
    Added new styles, now centered
    new options added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
+         cart: 0,
          product: "Socks",
+         brand: "Vue Mastery",
          description: "something asdadasd",
          image: './assets/images/socks_green.jpg',
          url: 'https://www.vuemastery.com/', 
@@ -9,10 +11,32 @@ const app = Vue.createApp({
          onSale: false,
          details: ['50% cotton', '30% whool', '20% polyester'],
          variants: [
-             {id: 2234, color: 'green'},
-             {id: 2235, color: 'blue'},
+             {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+             {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
          ],
          sizes: [24, 25, 26, 27, 28],
     }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product
+        }
+    },
+
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart() {
+            if(this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+                this.image = variantImage
+        }
     }
+
+
+
     })
