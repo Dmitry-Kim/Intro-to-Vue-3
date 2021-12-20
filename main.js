f7be121af27037719b605b18c255aa1844dfb406
Dmitry-Kim <itfwtf@gmail.com>
2021-12-20 11:49:13 +0600

    + stocks +sizes
    added stocks sizes with vue

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+         product: "Socks",
+         description: "something asdadasd",
+         image: './assets/images/socks_green.jpg',
+         url: 'https://www.vuemastery.com/', 
+         inventory: 2,
+         onSale: false,
+         details: ['50% cotton', '30% whool', '20% polyester'],
+         variants: [
+             {id: 2234, color: 'green'},
+             {id: 2235, color: 'blue'},
+         ],
+         sizes: [24, 25, 26, 27, 28],
+    }
+    }
+    })
